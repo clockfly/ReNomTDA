d1a3328159ff8b16616d10163eafb0ead4f36a9e
numajiri <numajiri@numa.local>
2017-11-28 11:47:53 +0900

    modify indent center components

--- gui/js/components/page/topology/center/Center.vue
+++ gui/js/components/page/topology/center/Center.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="center">
     <div id="canvas_header">
-        <button id="show_histogram_button" v-on:click="show_histogram"><i class="fa fa-bar-chart" aria-hidden="true"></i></button>
-        <button id="show_spring_button" v-on:click="show_spring"><i class="fa fa-expand" aria-hidden="true"></i></button>
-        <button id="reset_button" v-on:click="reset"><i class="fa fa-refresh" aria-hidden="true"></i></button>
+      <button id="show_histogram_button" v-on:click="show_histogram"><i class="fa fa-bar-chart" aria-hidden="true"></i></button>
+      <button id="show_spring_button" v-on:click="show_spring"><i class="fa fa-expand" aria-hidden="true"></i></button>
+      <button id="reset_button" v-on:click="reset"><i class="fa fa-refresh" aria-hidden="true"></i></button>
     </div>
 
     <!-- 1 columns -->
     <div v-if="columns == 0" class="canvas_area">
-        <d3canvas v-if="nodes && colors" :id="canvas_id" :width="width" :height="height" :nodes="nodes" :edges="edges" :colors="colors" :sizes="sizes" :trainindex="train_index" :histid="hist_id" :columns="'0'" :resolution="resolution"></d3canvas>
+      <d3canvas v-if="nodes && colors" :id="canvas_id" :width="width" :height="height" :nodes="nodes" :edges="edges" :colors="colors" :sizes="sizes" :trainindex="train_index" :histid="hist_id" :columns="'0'" :resolution="resolution"></d3canvas>
     </div>
 
     <!-- 2 columns -->
     <div v-if="columns == 1" class="canvas_area">
-        <div class="canvas_2column border-right">
-            <d3canvas v-if="nodes && colors" :id="canvas2_id" :width="width*0.5" :height="height" :nodes="nodes" :edges="edges" :colors="colors" :sizes="sizes" :trainindex="train_index" :histid="hist_id" :columns="'0'" :resolution="resolution"></d3canvas>
-        </div>
-        <div class="canvas_2column">
-            <d3canvas v-if="nodes_col2 && colors_col2" :id="canvas_id" :width="width*0.5" :height="height" :nodes="nodes_col2" :edges="edges_col2" :colors="colors_col2" :sizes="sizes_col2" :trainindex="train_index_col2" :histid="hist2_id" :columns="'1'" :resolution="resolution_col2"></d3canvas>
-        </div>
+      <div class="canvas_2column border-right">
+        <d3canvas v-if="nodes && colors" :id="canvas2_id" :width="width*0.5" :height="height" :nodes="nodes" :edges="edges" :colors="colors" :sizes="sizes" :trainindex="train_index" :histid="hist_id" :columns="'0'" :resolution="resolution"></d3canvas>
+      </div>
+      <div class="canvas_2column">
+        <d3canvas v-if="nodes_col2 && colors_col2" :id="canvas_id" :width="width*0.5" :height="height" :nodes="nodes_col2" :edges="edges_col2" :colors="colors_col2" :sizes="sizes_col2" :trainindex="train_index_col2" :histid="hist2_id" :columns="'1'" :resolution="resolution_col2"></d3canvas>
+      </div>
     </div>
   </div>
 </template>
@@ -27,119 +27,118 @@
 import D3Canvas from './D3Canvas.vue'
 
 export default {
-    name: "TopologyCenter",
-    data: function() {
-        return {
-            canvas_id: "canvas",
-            canvas2_id: "canvas2",
-            hist_id: "hist",
-            hist2_id: "hist2"
-        }
-    },
-    components: {
-        'd3canvas': D3Canvas
-    },
-    computed: {
-        width() {
-            let e = document.getElementById("center");
-            return e.clientWidth;
-        },
-        height() {
-            let e = document.getElementById("center");
-            return e.clientHeight*0.95;
-        },
-        columns() {
-            return this.$store.getters.layout_columns
-        },
-        resolution() {
-            return this.$store.state.topology.resolution
-        },
-        resolution_col2() {
-            return this.$store.state.topology.resolution_col2
-        },
-        nodes(){
-            return this.$store.state.topology.nodes
-        },
-        edges() {
-            return this.$store.state.topology.edges
-        },
-        colors() {
-            return this.$store.state.topology.colors
-        },
-        sizes() {
-            return this.$store.state.topology.sizes
-        },
-        train_index() {
-            return this.$store.state.topology.train_index
-        },
-        nodes_col2() {
-            return this.$store.state.topology.nodes_col2
-        },
-        edges_col2() {
-            return this.$store.state.topology.edges_col2
-        },
-        colors_col2() {
-            return this.$store.state.topology.colors_col2
-        },
-        sizes_col2() {
-            return this.$store.state.topology.sizes_col2
-        },
-        train_index_col2() {
-            return this.$store.state.topology.train_index_col2
-        }
-    },
-    methods: {
-        show_spring: function() {
-            document.getElementById("show_spring_button").classList.toggle('inverse');
-            this.$store.commit('set_show_spring');
-        },
-        show_histogram: function() {
-            document.getElementById("show_histogram_button").classList.toggle('inverse');
-            this.$store.commit('set_show_histogram');
-        },
-        reset: function() {
-            this.$store.dispatch('reset');
-        }
+  name: "TopologyCenter",
+  data: function() {
+    return {
+      canvas_id: "canvas",
+      canvas2_id: "canvas2",
+      hist_id: "hist",
+      hist2_id: "hist2"
     }
+  },
+  components: {
+    'd3canvas': D3Canvas
+  },
+  computed: {
+    width() {
+      let e = document.getElementById("center");
+      return e.clientWidth;
+    },
+    height() {
+      let e = document.getElementById("center");
+      return e.clientHeight*0.95;
+    },
+    columns() {
+      return this.$store.getters.layout_columns
+    },
+    resolution() {
+      return this.$store.state.topology.resolution
+    },
+    resolution_col2() {
+      return this.$store.state.topology.resolution_col2
+    },
+    nodes(){
+      return this.$store.state.topology.nodes
+    },
+    edges() {
+      return this.$store.state.topology.edges
+    },
+    colors() {
+      return this.$store.state.topology.colors
+    },
+    sizes() {
+      return this.$store.state.topology.sizes
+    },
+    train_index() {
+      return this.$store.state.topology.train_index
+    },
+    nodes_col2() {
+      return this.$store.state.topology.nodes_col2
+    },
+    edges_col2() {
+      return this.$store.state.topology.edges_col2
+    },
+    colors_col2() {
+      return this.$store.state.topology.colors_col2
+    },
+    sizes_col2() {
+      return this.$store.state.topology.sizes_col2
+    },
+    train_index_col2() {
+      return this.$store.state.topology.train_index_col2
+    }
+  },
+  methods: {
+    show_spring: function() {
+      document.getElementById("show_spring_button").classList.toggle('inverse');
+      this.$store.commit('set_show_spring');
+    },
+    show_histogram: function() {
+      document.getElementById("show_histogram_button").classList.toggle('inverse');
+      this.$store.commit('set_show_histogram');
+    },
+    reset: function() {
+      this.$store.dispatch('reset');
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 #center {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-}
-#canvas_header {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+
+  #canvas_header {
     width: 100%;
     height: 5%;
     #show_histogram_button, #show_spring_button, #reset_button {
-        height: 90%;
-        margin-top: 4px;
-        padding: 0 8px;
+      height: 90%;
+      margin-top: 4px;
+      padding: 0 8px;
     }
-}
-.canvas_1column {
+  }
+  .canvas_area {
+    height: 95%;
     width: 100%;
-    height: 100%;
-}
-.canvas_2column {
+    svg {
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .canvas_2column {
     float: left;
     width: 50%;
     height: 100%;
     margin: 0;
     padding: 0;
     box-sizing:border-box;
-}
-.canvas_area {
-    height: 95%;
-    width: 100%;
-    svg {
-        height: 100%;
-        width: 100%;
-    }
-}
-.border-right {
+  }
+
+  .border-right {
     border-right: 2px solid #eeeeee;
+  }
 }
 </style>
--- gui/js/components/page/topology/center/D3Histgram.vue
+++ gui/js/components/page/topology/center/D3Histgram.vue
@@ -6,113 +6,113 @@
 
 <script>
 export default {
-    name: "D3Histgram",
-    props: ["id"],
-    mounted: function(){
-        this.draw_graph();
-    },
-    methods: {
-        draw_graph: function() {
-            if (this.id === "hist"){
-                var data_color_values = this.$store.state.topology.data_color_values;
-                var statistic_value = this.$store.state.topology.statistic_value;
-            }else{
-                var data_color_values = this.$store.state.topology.data_color_values_col2;
-                var statistic_value = this.$store.state.topology.statistic_value_col2;
-            }
+  name: "D3Histgram",
+  props: ["id"],
+  mounted: function(){
+    this.draw_graph();
+  },
+  methods: {
+    draw_graph: function() {
+      if (this.id === "hist"){
+        let data_color_values = this.$store.state.topology.data_color_values;
+        let statistic_value = this.$store.state.topology.statistic_value;
+      }else{
+        let data_color_values = this.$store.state.topology.data_color_values_col2;
+        let statistic_value = this.$store.state.topology.statistic_value_col2;
+      }
 
-            let formatCount = d3.format(",.0f");
+      let formatCount = d3.format(",.0f");
 
-            let margin = {top: 10, right: 30, bottom: 50, left: 50};
-            let svg = d3.select('#'+this.id),
-                width = +svg.attr("width") - margin.left - margin.right,
-                height = +svg.attr("height") - margin.top - margin.bottom,
-                g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      let margin = {top: 10, right: 30, bottom: 50, left: 50};
+      let svg = d3.select('#'+this.id),
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var min_x = Math.min.apply(null, data_color_values)
-            var max_x = Math.max.apply(null, data_color_values)
-            let x = d3.scaleLinear()
-                .domain([min_x, max_x])
-                .rangeRound([0, width]);
+      let min_x = Math.min.apply(null, data_color_values)
+      let max_x = Math.max.apply(null, data_color_values)
+      let x = d3.scaleLinear()
+        .domain([min_x, max_x])
+        .rangeRound([0, width]);
 
-            let bins = d3.histogram()
-                .domain(x.domain())
-                .thresholds(d3.range(min_x, max_x, (max_x - min_x) / 20))
-                (data_color_values);
+      let bins = d3.histogram()
+        .domain(x.domain())
+        .thresholds(d3.range(min_x, max_x, (max_x - min_x) / 20))
+        (data_color_values);
 
-            let y = d3.scaleLinear()
-                .domain([0, d3.max(bins, function(d) { return d.length; })])
-                .range([height, 0]);
+      let y = d3.scaleLinear()
+        .domain([0, d3.max(bins, function(d) { return d.length; })])
+        .range([height, 0]);
 
-            let interpolation_hsv = function(value, q_min, q_max, val_min, val_max) {
-                var i = (value - val_min) / (val_max - val_min);
-                var h = i * 0.25 + q_min;
-                var hsv = d3.hsl((1-h)*240, 0.7, 0.5);
-                return hsv+"";
-            }
+      let interpolation_hsv = function(value, q_min, q_max, val_min, val_max) {
+        let i = (value - val_min) / (val_max - val_min);
+        let h = i * 0.25 + q_min;
+        let hsv = d3.hsl((1-h)*240, 0.7, 0.5);
+        return hsv+"";
+      }
 
-            let get_hsv_color = function(values) {
-                var sum = 0;
-                for(var i=0; i<values.length; i++){
-                    sum += values[i];
-                }
-                var avg = sum/values.length;
-
-                if(avg < statistic_value[0]){
-                    return interpolation_hsv(avg, 0, 0.25, min_x, statistic_value[0]);
-                }else if(avg > statistic_value[0] && avg < statistic_value[1]){
-                    return interpolation_hsv(avg, 0.25, 0.5, statistic_value[0], statistic_value[1]);
-                }else if(avg > statistic_value[1] && avg < statistic_value[2]){
-                    return interpolation_hsv(avg, 0.5, 0.75, statistic_value[1], statistic_value[2]);
-                }else{
-                    return interpolation_hsv(avg, 0.75, 1.0, statistic_value[2], max_x);
-                }
-            }
-
-            let bar = g.selectAll(".bar")
-                .data(bins)
-                .enter().append("g")
-                .attr("class", "bar")
-                .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-
-            bar.append("rect")
-                .attr("x", 1)
-                .attr("width", function(d) { return x(d.x1) - x(d.x0) - 2; })
-                .attr("height", function(d) { return height - y(d.length); })
-                .attr("fill", function(d) { return get_hsv_color(d); });
-
-            g.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x))
-             .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-.8em")
-                .attr("dy", ".15em")
-                .attr("transform", "rotate(-65)")
-                .style("font-size", "9px");
-
-            g.append("g")
-                    .call(d3.axisLeft(y))
-               .selectAll("text")
-               .style("text-anchor", "end")
-                .style("font-size", "9px");;
+      let get_hsv_color = function(values) {
+        let sum = 0;
+        for(let i=0; i<values.length; i++){
+            sum += values[i];
         }
+        let avg = sum/values.length;
+
+        if(avg < statistic_value[0]){
+          return interpolation_hsv(avg, 0, 0.25, min_x, statistic_value[0]);
+        }else if(avg > statistic_value[0] && avg < statistic_value[1]){
+          return interpolation_hsv(avg, 0.25, 0.5, statistic_value[0], statistic_value[1]);
+        }else if(avg > statistic_value[1] && avg < statistic_value[2]){
+          return interpolation_hsv(avg, 0.5, 0.75, statistic_value[1], statistic_value[2]);
+        }else{
+          return interpolation_hsv(avg, 0.75, 1.0, statistic_value[2], max_x);
+        }
+      }
+
+      let bar = g.selectAll(".bar")
+        .data(bins)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+      bar.append("rect")
+        .attr("x", 1)
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) - 2; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .attr("fill", function(d) { return get_hsv_color(d); });
+
+      g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)")
+        .style("font-size", "9px");
+
+      g.append("g")
+        .call(d3.axisLeft(y))
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .style("font-size", "9px");;
     }
+  }
 }
 </script>
 
 <style lang="scss">
 .d3histogram {
-    position: absolute;
-    top: 0;
-    right: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 210px;
+  height: 150px;
+  svg {
     width: 210px;
     height: 150px;
-    svg {
-        width: 210px;
-        height: 150px;
-    }
+  }
 }
 
 </style>
